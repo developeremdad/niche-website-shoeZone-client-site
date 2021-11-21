8171eb2e402a6674f80b74bbfc859e57732993a1
developeremdad <developeremdad@gmail.com>
2021-11-21 22:11:52 +0600

    implement swiperjs in review

--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -1,6 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Row, Col, Spinner } from 'react-bootstrap';
+import { Card, Spinner } from 'react-bootstrap';
 import ReactStars from 'react-stars';
+import './Reviews.css';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+    EffectCoverflow, Pagination
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([EffectCoverflow, Pagination]);
 
 const Reviews = () => {
     const [reviews, setReviews] = useState([]);
@@ -18,38 +33,58 @@ const Reviews = () => {
                 </div>
 
                 <div className="container">
-                    <Row xs={1} sm={2} md={3} lg={4} className="g-3">
-                        {
-                            reviews.length
-                                ?
-                                reviews.map(review => <Col
-                                    key={review?._id}
-                                >
-                                    <Card style={{ height: '200px' }} className="border">
-                                        {/* <Card.Header style={{ color: '#87b106', fontSize: '20px', backgroundColor: '#000' }}>Website Review ({Math.floor(Math.random() * 100)}) <span><i className="fas fa-users text-primary"></i></span> </Card.Header> */}
-                                        <Card.Body className="text-start">
-                                            <Card.Title><span><i className="fas fa-user text-primary"></i></span> {review?.userName.toUpperCase()}</Card.Title>
-                                            <Card.Text>
-                                                {review?.review.split(' ').slice(0, 15).toString().replace(/,/g, ' ')}
-                                            </Card.Text>
-                                        </Card.Body>
-                                        <Card.Footer>
-                                            <div className="d-flex justify-content-between align-items-center">
-                                                {<ReactStars
-                                                    count={5}
-                                                    value={parseInt(review?.rating)}
-                                                    size={28}
-                                                    color2={'#ffd700'} />
-                                                }
-                                                <span>({Math.floor(Math.random() * 100)}) <span><i className="fas fa-users text-primary"></i></span> </span>
-                                            </div>
-                                        </Card.Footer>
-                                    </Card>
-                                </Col>)
-                                :
-                                <div className="text-center mt-3 mx-auto mt-5"><Spinner animation="border" variant="danger" /></div>
-                        }
-                    </Row>
+
+
+
+                    <>
+                        <Swiper effect={'coverflow'} grabCursor={true} centeredSlides={true} slidesPerView={'auto'} coverflowEffect={{
+                            "rotate": 50,
+                            "stretch": 0,
+                            "depth": 100,
+                            "modifier": 1,
+                            "slideShadows": true
+                        }} pagination={true} className="mySwiper">
+
+
+                            {
+                                reviews.length
+                                    ?
+                                    reviews.map(review => <SwiperSlide
+                                        key={review?._id}
+                                    >
+
+                                        <Card className="border h-75">
+                                            {/* <Card.Header style={{ color: '#87b106', fontSize: '20px', backgroundColor: '#000' }}>Website Review ({Math.floor(Math.random() * 100)}) <span><i className="fas fa-users text-primary"></i></span> </Card.Header> */}
+                                            <Card.Body className="text-start">
+                                                <Card.Title><span><i className="fas fa-user text-primary"></i></span> {review?.userName.toUpperCase()}</Card.Title>
+                                                <Card.Text>
+                                                    {review?.review.split(' ').slice(0, 15).toString().replace(/,/g, ' ')}
+                                                </Card.Text>
+                                            </Card.Body>
+                                            <Card.Footer>
+                                                <div className="d-flex justify-content-between align-items-center">
+                                                    {<ReactStars
+                                                        count={5}
+                                                        value={parseInt(review?.rating)}
+                                                        size={28}
+                                                        color2={'#ffd700'} />
+                                                    }
+                                                    <span>({Math.floor(Math.random() * 100)}) <span><i className="fas fa-users text-primary"></i></span> </span>
+                                                </div>
+                                            </Card.Footer>
+                                        </Card>
+
+
+                                    </SwiperSlide>)
+                                    :
+                                    <div className="text-center mt-3 mx-auto mt-5"><Spinner animation="border" variant="danger" /></div>
+                            }
+
+                        </Swiper>
+                    </>
+
+
+
                 </div>
             </div>
         </div>
